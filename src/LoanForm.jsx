4334df5b7a224e8949aeb5524f7a195839f62006
amen <ramadanamen407@gmail.com>
2024-03-03 23:24:57 +0300

    finish

--- src/LoanForm.jsx
+++ src/LoanForm.jsx
@@ -1,39 +1,118 @@
 import "./FormStyle.css";
+import { useState } from "react";
 import Modal from "./Modal";
 
 export default function LoanForm() {
+  const [errorMassage, setErrorMassage] = useState(null);
+  const [loneInputs, setLoneInputs] = useState({
+    name: "",
+    phoneNumber: "",
+    age: "",
+    isEmploy: false,
+    salaryRange: "less than 500$",
+  });
+
+  const [showModel, setShowModel] = useState(false);
+
+  const btnIsDisable =
+    loneInputs.name == "" ||
+    loneInputs.age == "" ||
+    loneInputs.phoneNumber == "";
+
+  function handleFormSubmit(event) {
+    setErrorMassage(null); // هذا بمثابة تصفير للستيت الخاصة بارسال الاخطاء ك بروب للموديل لانه اذا لم يتم تصفيرها .. عند تعبئة بيانات خاطئة وارسال الفورم والرجوع عن الخطأ وتصحيح البيانات فإنه لايتم حذف رسالة الخطأ
+    event.preventDefault();
+
+    const { age } = loneInputs;
+
+    if (age < 18 || age > 100) {
+      setErrorMassage("the age is not allowed");
+    }
+    const { phoneNumber } = loneInputs;
+    if (phoneNumber.length < 10 || phoneNumber.length > 12) {
+      setErrorMassage("phone number formate is incorrect");
+    }
+    setShowModel(true);
+  }
+
+  function handleDivClick() {
+    if (showModel) {
+      setShowModel(false);
+    }
+  }
+
   return (
-    <div className="flex">
+    <div className="flex" onClick={handleDivClick}>
       <form className="flex" style={{ flexDirection: "column" }}>
         <h1>Requesting a Loan</h1>
         <hr></hr>
 
         <label>Name:</label>
-        <input type="text" />
+        <input
+          type="text"
+          value={loneInputs.name}
+          onChange={(event) => {
+            setLoneInputs({ ...loneInputs, name: event.target.value });
+          }}
+        />
 
         <label>Phone Number:</label>
-        <input type="number" />
+        <input
+          type="number"
+          value={loneInputs.phoneNumber}
+          onChange={(event) => {
+            setLoneInputs({ ...loneInputs, phoneNumber: event.target.value });
+          }}
+        />
 
         <label>Age:</label>
-        <input type="number" />
+        <input
+          type="number"
+          value={loneInputs.age}
+          onChange={(event) => {
+            setLoneInputs({ ...loneInputs, age: event.target.value });
+          }}
+        />
 
         <hr />
 
         <label>Are you an employee?</label>
-        <input type="checkbox" />
+        <input
+          type="checkbox"
+          checked={loneInputs.isEmploy}
+          onChange={(event) => {
+            setLoneInputs({ ...loneInputs, isEmploy: event.target.checked });
+          }}
+        />
 
         <hr />
 
         <label>Salary</label>
-        <select>
+        <select
+          value={loneInputs.salaryRange}
+          onChange={(event) => {
+            setLoneInputs({ ...loneInputs, salaryRange: event.target.value });
+          }}
+        >
           <option>less than 500$</option>
           <option>between 500$ and 2000$</option>
           <option>above 2000$</option>
         </select>
 
-        <button>Submit</button>
+        <button
+          className={btnIsDisable ? "disabled" : ""}
+          disabled={btnIsDisable}
+          onClick={handleFormSubmit}
+          // style={
+          //   btnIsDisable
+          //     ? { backgroundColor: "green" }
+          //     : {}
+          // }
+        >
+          Submit
+        </button>
       </form>
-      <Modal />
+      <Modal isVisible={showModel} errorMassage={errorMassage} />
     </div>
   );
 }
--- src/Modal.jsx
+++ src/Modal.jsx
@@ -1,10 +1,18 @@
 import "./FormStyle.css";
-export default function Modal() {
-  return (
-    <div id="modal">
-      <div id="modal-content">
-        <h1>The Form Has Been Submitted Successfully</h1>
+export default function Modal({ isVisible, errorMassage = null }) {
+  if (isVisible) {
+    return (
+      <div id="modal">
+        <div id="modal-content">
+          <h1 style={{ color: errorMassage ? "red" : "green" }}>
+            {errorMassage != null
+              ? errorMassage
+              : "The Form Has Been Submitted Successfully"}
+          </h1>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return <></>;
+  }
 }
